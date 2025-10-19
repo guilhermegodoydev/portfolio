77a4bb8a99a0de88cf69ddbab036204451cc5324
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-10-19 17:24:03 -0300

    Adicionando c# a lista de stacks

--- main.js
+++ main.js
@@ -12,95 +12,102 @@ const nav = document.querySelector("nav");
 let versaoMobile = false;
 let tecAtual = tecnologias[4];
 let infoTecnologias = {
-  "html": {
+  html: {
     nome: "HTML 5",
-    descricao: "Criação de páginas web semânticas e responsivas, incluindo formulários, multimídia e integração com CSS/JS.",
+    descricao:
+      "Criação de páginas web semânticas e responsivas, incluindo formulários, multimídia e integração com CSS/JS.",
   },
 
-  "css": {
+  css: {
     nome: "CSS 3",
-    descricao: "Estilização de páginas web modernas e responsivas, utilizando layouts flexíveis, grid, animações, transições e design adaptativo para diferentes dispositivos.",
+    descricao:
+      "Estilização de páginas web modernas e responsivas, utilizando layouts flexíveis, grid, animações, transições e design adaptativo para diferentes dispositivos.",
   },
 
-  "js": {
+  js: {
     nome: "JavaScript",
-    descricao: "Desenvolvimento de interatividade e funcionalidades dinâmicas em páginas web, manipulação do DOM, requisições a APIs, validação de formulários e implementação de lógica de front-end.",
+    descricao:
+      "Desenvolvimento de interatividade e funcionalidades dinâmicas em páginas web, manipulação do DOM, requisições a APIs, validação de formulários e implementação de lógica de front-end.",
   },
 
-  "node": {
-    nome: "Node",
-    descricao: "Desenvolvimento de aplicações back-end escaláveis e rápidas, criação de APIs RESTful, manipulação de arquivos, integração com bancos de dados usando JavaScript no lado do servidor.",
+  csharp: {
+    nome: "Csharp (C#)",
+    descricao:
+      "Desenvolvimento de APIs REST utilizando C# e ASP.NET, com foco em desempenho e organização de código. Aplicação de princípios de programação orientada. Integração com banco de dados MySQL, garantindo eficiência e segurança nas operações.",
   },
 
-  "react": {
+  react: {
     nome: "React",
-    descricao: "Construção de interfaces de usuário dinâmicas e reutilizáveis, gerenciamento de estado com hooks, criação de componentes moduláveis e integração com APIs.",
+    descricao:
+      "Construção de interfaces de usuário dinâmicas e reutilizáveis, gerenciamento de estado com hooks, criação de componentes moduláveis e integração com APIs.",
   },
 
-  "tailwind": {
+  tailwind: {
     nome: "Tailwind",
-    descricao: "Desenvolvimento de interfaces web responsivas e organizadas usando classes utilitárias, com controle de estilos em camadas (layers) para design modular, manutenção fácil e prototipagem rápida.",
-  }
+    descricao:
+      "Desenvolvimento de interfaces web responsivas e organizadas usando classes utilitárias, com controle de estilos em camadas (layers) para design modular, manutenção fácil e prototipagem rápida.",
+  },
 };
 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+particlesJS.load("particles-js", "assets/particles.json", function () {
+  console.log("callback - particles.js config loaded");
 });
 
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      header.classList.add("header-scroll");
+  entries.forEach(
+    (entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("header-scroll");
+      } else {
+        header.classList.remove("header-scroll");
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
     }
-    else {
-      header.classList.remove("header-scroll");
-    }
-  }, {
-    root: null,
-    threshold: 0,
-  });
+  );
 });
 
 observer.observe(banner);
 
-tecnologias.forEach(tec => {
-  tec.addEventListener('click', () => {
+tecnologias.forEach((tec) => {
+  tec.addEventListener("click", () => {
     if (tec != tecAtual) {
       informacoesTec = infoTecnologias[tec.getAttribute("data-id")];
       nomeTecnologia.textContent = informacoesTec.nome;
       imgTecnologia.setAttribute("src", tec.getAttribute("src"));
       imgTecnologia.setAttribute("alt", infoTecnologias.alt);
-      descTecnologia.textContent = informacoesTec.descricao
-      
+      descTecnologia.textContent = informacoesTec.descricao;
+
       console.log(tec);
       tec.classList.add("selecionada");
       tecAtual.classList.remove("selecionada");
-      
+
       tecAtual = tec;
     }
   });
 });
 
-abrirMenu.addEventListener('click', () => {
+abrirMenu.addEventListener("click", () => {
   fecharMenu.classList.remove("escondido");
   header.classList.remove("escondido");
   abrirMenu.classList.add("escondido");
 });
 
-fecharMenu.addEventListener('click', () => {
+fecharMenu.addEventListener("click", () => {
   esconderMenu();
 });
 
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    if (versaoMobile)
-      esconderMenu();
-  })
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (versaoMobile) esconderMenu();
+  });
 });
 
-window.addEventListener('load', alterarVisibilidadeMenu);
+window.addEventListener("load", alterarVisibilidadeMenu);
 
-window.addEventListener('resize', alterarVisibilidadeMenu);
+window.addEventListener("resize", alterarVisibilidadeMenu);
 
 function alterarVisibilidadeMenu() {
   if (window.innerWidth <= 700) {
@@ -108,8 +115,7 @@ function alterarVisibilidadeMenu() {
     header.classList.add("escondido");
     abrirMenu.classList.remove("escondido");
     fecharMenu.classList.remove("escondido");
-  }
-  else {
+  } else {
     versaoMobile = false;
     header.classList.remove("escondido");
     abrirMenu.classList.add("escondido");
